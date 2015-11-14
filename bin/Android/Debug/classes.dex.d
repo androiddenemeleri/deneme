9b96d2e6b5109c697a326528b42149e71f96e53e
androiddenemeleri <androiddenemeleri@gmail.com>
2015-11-14 20:53:20 +0200

    fcf

---
+++ bin/Android/Debug/classes.dex.d
@@ -0,0 +1,12 @@
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes.dex : \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\BuildConfig.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\MainActivity.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\R$attr.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\R$drawable.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\R$string.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\R$xml.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\io\cordova\IlkTest\R.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\org\apache\cordova\camera\CameraLauncher.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\org\apache\cordova\camera\ExifHelper.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes\org\apache\cordova\camera\FileHelper.class \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\dexedLibs\classes-b5b20cbfdf4dbcd2f32e6b389e4bcf03.jar \
