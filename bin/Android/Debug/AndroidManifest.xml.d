b009a68c20f33ad12e44db3f2be57a562756a7ac
androiddenemeleri <androiddenemeleri@gmail.com>
2015-11-14 21:02:42 +0200

    gf

--- bin/Android/Debug/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\AndroidManifest.xml : \
-C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\AndroidManifest.xml \
