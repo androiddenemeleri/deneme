9171190b4d802454e79d1ba42dd3da933904f86b
androiddenemeleri <androiddenemeleri@gmail.com>
2015-11-14 21:04:17 +0200

    hjkghkjhk

---
+++ bin/Android/Debug/MainActivity-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\MainActivity-debug-unaligned.apk : \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\MainActivity.ap_ \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\ant-build\classes.dex \
+C:\Users\CODER\Documents\Visual Studio 2013\Projects\IlkTest\IlkTest\bld\Debug\platforms\android\CordovaLib\ant-build\classes.jar \
